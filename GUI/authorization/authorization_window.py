bfbfa13db8425d8fc1b8d8db2cfc308ec5670732
Yury <yura.shamanov2000@gmail.com>
2023-07-10 09:38:24 +0200

    refactor classes

--- GUI/authorization/authorization_window.py
+++ GUI/authorization/authorization_window.py
@@ -1,28 +1,17 @@
 from tkinter import *
-
-from templates.form import InputForm
-from data_verification.chek import check_existence_login
+from GUI.templates import BaseInputWindow
 
 
-#ПРИ НАЖАТИИ АВТОРИЗАЦИЯ
-def authorization():
-    window = Tk()
-    window.title("Secret Notepad (АВТОРИЗАЦИЯ)")
-    window.geometry('600x400')
+# ПРИ НАЖАТИИ АВТОРИЗАЦИЯ
+class AuthorizationWindow(BaseInputWindow):
+    def __init__(self):
+        super().__init__("АВТОРИЗАЦИЯ", "600x800")
 
-    login_form = InputForm(window, 'ЛОГИН', 30)
-    login_form.pack(pady=15)
-
-    password_form = InputForm(window, 'ПАРОЛЬ', 30, show='*')
-    password_form.pack(pady=15)
-
-    login_but = Button(window, width=15, text='ВОЙТИ',
-                  relief=SOLID, cursor='hand2', font=('Arial', 10), command=NONE)
-    login_but.pack(pady=15)
-
-
-
-    window.mainloop()
-
-if __name__ == '__main__':
-    authorization()
+        self.__login_but = Button(self,
+                                  width=15,
+                                  text='ВОЙТИ',
+                                  relief=SOLID,
+                                  cursor='hand2',
+                                  font=('Arial', 10),
+                                  command=None)
+        self.__login_but.pack(pady=15)
--- GUI/registration/registration_window.py
+++ GUI/registration/registration_window.py
@@ -1,27 +1,27 @@
 from tkinter import *
-from GUI.templates import InputForm, BaseWindow
+from GUI.templates import InputForm, BaseInputWindow
 
 
-class RegistrationWindow(BaseWindow):
-    def __init__(self, title: str, geometry: str):
+class RegistrationWindow(BaseInputWindow):
+    def __init__(self):
         super().__init__("РЕГИСТРАЦИЯ", "600x800")
-        login_form = InputForm(self, 'ЛОГИН', 30)
-        login_form.pack(pady=15)
 
-        password_form = InputForm(self, 'ПАРОЛЬ', 30, show='*')
-        password_form.pack(pady=15)
+        self.__password_form2 = InputForm(self, 'ПОВТОРИТЕ ПАРОЛЬ', 30, show='*')
+        self.__password_form2.pack(pady=12)
 
-        password_form2 = InputForm(self, 'ПОВТОРИТЕ ПАРОЛЬ', 30, show='*')
-        password_form2.pack(pady=12)
+        self.__login_but = Button(self,
+                                  width=15,
+                                  text='СОХРАНИТЬ',
+                                  relief=SOLID,
+                                  cursor='hand2',
+                                  font=('Arial', 10),
+                                  command=None)
+        self.__login_but.pack(pady=15)
 
-        login_but = Button(self,
-                           width=15,
-                           text='СОХРАНИТЬ',
-                           relief=SOLID,
-                           cursor='hand2',
-                           font=('Arial', 10),
-                           command=None)
-        login_but.pack(pady=15)
+    def get(self):
+        result = self.get()
+        result.append(self.__password_form2.get())
+        return result
 
 
 if __name__ == '__main__':
